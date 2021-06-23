cc588c8ad56a4dcf54df608541888c1df341d872
MasayaEzure <h7t1s2@gmail.com>
2021-06-23 18:50:03 +0900

    ファイルを

--- lesson2.rb
+++ lesson2.rb
@@ -12,14 +12,3 @@ p 10.6.round
 p 10.6.floor
 # 小数点以下を切り上げ
 p 10.6.ceil
-
-# ""は、特殊文字と式展開が可能
-puts "hello\n worl\td"
-puts "number #{10 * 3}"
-
-# すべて文字列として認識される
-puts 'hello\n worl\td'
-puts 'number #{10 * 3}'
-
-# 繰り返し処理が可能
-puts "abcde " * 10
---
+++ lesson2_1.rb
@@ -0,0 +1,10 @@
+# ""は、特殊文字と式展開が可能
+puts "hello\n worl\td"
+puts "number #{10 * 3}"
+
+# すべて文字列として認識される
+puts 'hello\n worl\td'
+puts 'number #{10 * 3}'
+
+# 繰り返し処理が可能
+puts "abcde " * 10
